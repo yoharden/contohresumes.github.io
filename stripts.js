33db161b5fdb51f22e87e22fa02865024394ee0e
li jason <jl0301429@gmail.com>
2023-10-14 22:56:19 +0800

    Update and rename stript.js to stripts.js

---
+++ stripts.js
@@ -0,0 +1,35 @@
+// JavaScript File: scripts.js
+
+// Back to Top Button
+const backToTopButton = document.createElement('button');
+backToTopButton.innerText = '回到顶部';
+backToTopButton.id = 'back-to-top';
+document.body.appendChild(backToTopButton);
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
